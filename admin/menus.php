57310737c32bca7f3553d92221ee7b13d0765b0a
Danielhardin <danielhardin581@gmail.com>
2023-05-23 13:49:42 +0630

    menu image added.

--- admin/menus.php
+++ admin/menus.php
@@ -133,6 +133,67 @@
                  </script>";
         }
     }
+    if(isset($_POST['btnUpdateImage']))
+    {
+            // file upload
+            $target_file = $target_dir . basename($_FILES["uimage"]["name"]);
+            $uploadOk = 1;
+            $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+            $check = getimagesize($_FILES["uimage"]["tmp_name"]);
+            if($check === false) {
+                echo "<script>
+                        alert('File is not an image.')
+                    </script>";
+            }
+            
+            // Check if file already exists
+            elseif (file_exists($target_file)) {
+            echo "<script>
+                    alert('Sorry, file already exists.')
+                </script>";
+            }
+
+            // Check file size
+            elseif ($_FILES["image"]["size"] > 500000) {
+            echo "<script>
+                    alert('Sorry, your file is too large.')
+                </script>";
+            }
+
+            // Allow certain file formats
+            elseif($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo "<script>
+                    alert('Sorry, only JPG, JPEG, PNG & GIF files are allowed.')
+                </script>";
+            }
+        elseif (move_uploaded_file($_FILES["uimage"]["tmp_name"], $target_file)) {
+                $image = $_FILES["uimage"]["name"];
+                $menu_id = $_POST['umenu_id'];
+                $old_image = $_POST['old_image'];
+                $update = "UPDATE menus SET
+                           image = '$image'
+                           WHERE id = '$menu_id'
+                          ";
+                $update_query = mysqli_query($connect,$update);
+                if($update_query)
+                {
+                    unlink($target_dir.$old_image);
+                    echo "<script>
+                            alert('Menu image updated successfully')
+                            window.location.assign('menus.php')
+                          </script>
+                        ";
+                }
+            } else {
+                echo "<script>
+                        alert('Sorry, there was an error uploading your file.')
+                        window.location.assign('menus.php')
+                    </script>
+                    ";
+            }
+            
+    }
 ?>
       <div class="panel-header panel-header-sm">
       </div>
@@ -173,10 +234,7 @@
                                     <div class="flex">
                                         <button class="btn btn-primary editbtn" data-toggle="modal" data-target="#editModal" data-menu='<?php echo json_encode($row_menu); ?>'>Edit</button>
                                         <button class="btn btn-danger deletebtn" data-toggle="modal" data-target="#deleteModal" data-id="<?php echo $row_menu['id']; ?>">Delete</button>
-                                    </div>
-                                    <div class="flex mt-3">
-                                        <button class="btn btn-primary editbtn" data-toggle="modal" data-target="#editModal" data-menu='<?php echo json_encode($row_menu); ?>'>Update Image</button>
-                                        <button class="btn btn-danger deletebtn" data-toggle="modal" data-target="#deleteModal" data-id="<?php echo $row_menu['id']; ?>">Feature Images</button>
+                                        <button class="btn btn-primary editimagebtn" data-toggle="modal" data-target="#editimageModal" data-menu='<?php echo json_encode($row_menu); ?>'>Update Image</button>
                                     </div>
                                 </td>
                             </tr>
@@ -310,6 +368,35 @@
       </form>
     </div>
   </div>
+  <!-- Edit Modal -->
+<div class="modal fade" id="editimageModal" role="dialog">
+    <div class="modal-dialog">
+        <form action="menus.php" method="POST" enctype="multipart/form-data">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title float-left">Update Image</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                <div class="row">
+                        <div class="col-md-6">
+                            <label for="uimage">Image</label>
+                            <input type="file" class="form-control" name="uimage" id="uimage" placeholder="Upload Image" required>
+                        </div>
+                    </div>
+                    <input type="hidden" name="old_image" id="old_image">
+                    <input type="hidden" name="umenu_id" id="umenuimage_id">
+                </div>
+                
+                <div class="modal-footer">
+                <button type="submit" class="btn btn-primary" name="btnUpdateImage">Save</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+      </form>
+    </div>
+  </div>
   <!-- Delete Modal -->
   <div class="modal fade" id="deleteModal" role="dialog">
     <div class="modal-dialog">
@@ -347,6 +434,12 @@
                 $('#ucategory_id').val(menu_data_obj.category_id);
                 $('#uprice').val(menu_data_obj.price);
                 $('#umenu_id').val(menu_data_obj.id);
+            });
+            $(".editimagebtn").click(function(){
+                var menu_data = $(this).attr("data-menu");
+                var menu_data_obj = $.parseJSON(menu_data);
+                $('#old_image').val(menu_data_obj.image);
+                $('#umenuimage_id').val(menu_data_obj.id);
             });
             $(".deletebtn").click(function(){
                 var menu_id = $(this).attr("data-id");
